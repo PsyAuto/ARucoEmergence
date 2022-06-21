0773df0660f0a2410654423bbfc4478403346778
Marios Iakovidis <marios.iakovidis@gmail.com>
2022-06-21 04:50:06 +0300

    initial commit

---
+++ Assets/ArucoUnity/Scripts/MoreOptions.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoreOptions : MonoBehaviour
+{
+    public char myName;
+    public float interactionRadius;
+    public char wikiBox;
+    public char pictures;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
